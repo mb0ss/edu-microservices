779824eeff562c385f0d137604c29a53a1c326b0
Mateusz Wysokiński <mateusz.wysokinski@altkomsoftware.pl>
2019-04-08 11:02:46 +0200

    init maven modules

---
+++ caseservice/caseservice-web/src/main/java/pl/altkom/edu/mwysokinski/caseservice/web/PolicyServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.altkom.edu.mwysokinski.policyservice.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolicyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolicyServiceApplication.class, args);
+	}
+
+}
