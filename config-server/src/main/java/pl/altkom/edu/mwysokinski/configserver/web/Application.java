8aa2c0532108e69c43a18ff95484f5a4d894b2c2
Mateusz Wysokiński <mateusz.wysokinski@altkomsoftware.pl>
2019-04-09 11:57:03 +0200

    init config server

---
+++ config-server/src/main/java/pl/altkom/edu/mwysokinski/configserver/web/Application.java
@@ -0,0 +1,15 @@
+package pl.altkom.edu.mwysokinski.caseservice.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
