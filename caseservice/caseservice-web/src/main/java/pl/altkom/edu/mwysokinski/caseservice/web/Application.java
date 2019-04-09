ef4a5de391085fb572c7e863c5aa282b99582bde
Mateusz Wysokiński <mateusz.wysokinski@altkomsoftware.pl>
2019-04-09 15:04:10 +0200

    consul discovery

--- caseservice/caseservice-web/src/main/java/pl/altkom/edu/mwysokinski/caseservice/web/Application.java
+++ caseservice/caseservice-web/src/main/java/pl/altkom/edu/mwysokinski/caseservice/web/Application.java
@@ -2,8 +2,10 @@ package pl.altkom.edu.mwysokinski.caseservice.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Application {
 
 	public static void main(String[] args) {
--- policyservice/policyservice-web/src/main/java/pl/altkom/edu/mwysokinski/policyservice/web/Application.java
+++ policyservice/policyservice-web/src/main/java/pl/altkom/edu/mwysokinski/policyservice/web/Application.java
@@ -2,11 +2,13 @@ package pl.altkom.edu.mwysokinski.policyservice.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
 @EnableScheduling
+@EnableDiscoveryClient
 public class Application {
 
     public static void main(String[] args) {
