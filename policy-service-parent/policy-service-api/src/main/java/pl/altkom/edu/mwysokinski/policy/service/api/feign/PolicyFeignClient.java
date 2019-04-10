0eee25e7e91d1f362f2938a16f390e6a80110b3a
Mateusz Wysokiński <mateusz.wysokinski@altkomsoftware.pl>
2019-04-10 23:56:50 +0200

    change api url

--- policy-service-parent/policy-service-api/src/main/java/pl/altkom/edu/mwysokinski/policy/service/api/feign/PolicyFeignClient.java
+++ policy-service-parent/policy-service-api/src/main/java/pl/altkom/edu/mwysokinski/policy/service/api/feign/PolicyFeignClient.java
@@ -3,7 +3,6 @@ package pl.altkom.edu.mwysokinski.policy.service.api.feign;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import pl.altkom.edu.mwysokinski.policy.service.api.dto.PolicyDto;
 
 @FeignClient("policy-service")
@@ -12,7 +11,7 @@ public interface PolicyFeignClient {
     @GetMapping("/rest/{policyNumber}")
     PolicyDto getPolicyByNumber(@PathVariable("policyNumber") String policyNumber);
 
-    @RequestMapping("/rest/{policyNumber}/{date}")
+    @GetMapping("/rest/{policyNumber}/{date}")
     PolicyDto getPolicyByNumberAndDate(@PathVariable("policyNumber") String policyNumber, @PathVariable("date") String date);
 
 }
