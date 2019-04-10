eec7dec5f70cf81e53f61be46f7924e3ce983a11
Mateusz Wysokiński <mateusz.wysokinski@altkomsoftware.pl>
2019-04-10 23:49:02 +0200

    fix

--- claim-service-parent/claim-service-web/src/main/java/pl/altkom/edu/mwysokinski/claim/service/web/proxy/PolicyService.java
+++ claim-service-parent/claim-service-web/src/main/java/pl/altkom/edu/mwysokinski/claim/service/web/proxy/PolicyService.java
@@ -1,7 +1,9 @@
 package pl.altkom.edu.mwysokinski.claim.service.web.proxy;
 
+import org.springframework.cloud.openfeign.FeignClient;
 import pl.altkom.edu.mwysokinski.policy.service.api.feign.PolicyFeignClient;
 
+@FeignClient("policy-service")
 public interface PolicyService extends PolicyFeignClient {
 
 }
